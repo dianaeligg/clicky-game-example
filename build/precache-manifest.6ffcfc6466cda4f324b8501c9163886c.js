526ba7d5f67fc19c08d438b527f3b7bba795c816
Diana García <diana.eli.gg@gmail.com>
2020-02-04 18:59:03 -0600

    Added build

---
+++ build/precache-manifest.6ffcfc6466cda4f324b8501c9163886c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "284d6798866f76735a11",
+    "url": "/clicky-game-example/static/js/runtime~main.fba4c214.js"
+  },
+  {
+    "revision": "fddf7a4aab81bf5e3c47",
+    "url": "/clicky-game-example/static/js/main.51a73d9b.chunk.js"
+  },
+  {
+    "revision": "49db0d9fc976f003b3ff",
+    "url": "/clicky-game-example/static/js/2.458af4f8.chunk.js"
+  },
+  {
+    "revision": "fddf7a4aab81bf5e3c47",
+    "url": "/clicky-game-example/static/css/main.97f68ef5.chunk.css"
+  },
+  {
+    "revision": "ab9413795bb0400139a8270a3e4227c3",
+    "url": "/clicky-game-example/index.html"
+  }
+];
